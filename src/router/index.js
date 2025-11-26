1d2a4b4c4abefcc21df7ab6abbcd639f11d6a347
Ade Tio Sopian <adetiosopian@gmail.com>
2025-11-26 15:13:41 +0700

    g

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Tampilan from '../components/tampilan.vue'
 import GiftBox3D from '../components/GiftBox3D.vue'
-import dfss from '../components/dfss.vue'
+
 
 
 
@@ -12,7 +12,7 @@ import dfss from '../components/dfss.vue'
 const routes = [
   {  path: '/', component:GiftBox3D},
   {  path: '/tampilan', component:Tampilan},
-  {path: '/h', component:dfss}
+
 
 
 
